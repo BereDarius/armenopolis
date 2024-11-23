347605c69277a8d90c7163a55262aa547782b18a
Darius-Bogdan Bere <darius.bogdan3080@gmail.com>
2024-11-23 11:23:58 +0200

    chore: Add alt to images

--- app/galerie/page.tsx
+++ app/galerie/page.tsx
@@ -15,59 +15,99 @@ import image11 from "../../public/images/image-11.jpg";
 
 export default function GaleriePage() {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+    <section className="grid grid-cols-2 md:grid-cols-4 gap-4">
       <div className="grid gap-4">
         <div>
-          <Image alt="" className="h-auto max-w-full rounded-lg" src={image} />
-        </div>
-        <div>
-          <Image alt="" className="h-auto max-w-full rounded-lg" src={image1} />
-        </div>
-        <div>
-          <Image alt="" className="h-auto max-w-full rounded-lg" src={image2} />
-        </div>
-      </div>
-      <div className="grid gap-4">
-        <div>
-          <Image alt="" className="h-auto max-w-full rounded-lg" src={image3} />
-        </div>
-        <div>
-          <Image alt="" className="h-auto max-w-full rounded-lg" src={image4} />
-        </div>
-        <div>
-          <Image alt="" className="h-auto max-w-full rounded-lg" src={image5} />
-        </div>
-      </div>
-      <div className="grid gap-4">
-        <div>
-          <Image alt="" className="h-auto max-w-full rounded-lg" src={image6} />
-        </div>
-        <div>
-          <Image alt="" className="h-auto max-w-full rounded-lg" src={image7} />
-        </div>
-        <div>
-          <Image alt="" className="h-auto max-w-full rounded-lg" src={image8} />
-        </div>
-      </div>
-      <div className="grid gap-4">
-        <div>
-          <Image alt="" className="h-auto max-w-full rounded-lg" src={image9} />
+          <Image
+            alt="image"
+            className="h-auto max-w-full rounded-lg"
+            src={image}
+          />
         </div>
         <div>
           <Image
-            alt=""
+            alt="image1"
+            className="h-auto max-w-full rounded-lg"
+            src={image1}
+          />
+        </div>
+        <div>
+          <Image
+            alt="image2"
+            className="h-auto max-w-full rounded-lg"
+            src={image2}
+          />
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <div>
+          <Image
+            alt="image3"
+            className="h-auto max-w-full rounded-lg"
+            src={image3}
+          />
+        </div>
+        <div>
+          <Image
+            alt="image4"
+            className="h-auto max-w-full rounded-lg"
+            src={image4}
+          />
+        </div>
+        <div>
+          <Image
+            alt="image5"
+            className="h-auto max-w-full rounded-lg"
+            src={image5}
+          />
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <div>
+          <Image
+            alt="image6"
+            className="h-auto max-w-full rounded-lg"
+            src={image6}
+          />
+        </div>
+        <div>
+          <Image
+            alt="image7"
+            className="h-auto max-w-full rounded-lg"
+            src={image7}
+          />
+        </div>
+        <div>
+          <Image
+            alt="image8"
+            className="h-auto max-w-full rounded-lg"
+            src={image8}
+          />
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <div>
+          <Image
+            alt="image9"
+            className="h-auto max-w-full rounded-lg"
+            src={image9}
+          />
+        </div>
+        <div>
+          <Image
+            alt="image10"
             className="h-auto max-w-full rounded-lg"
             src={image10}
           />
         </div>
         <div>
           <Image
-            alt=""
+            alt="image11"
             className="h-auto max-w-full rounded-lg"
             src={image11}
           />
         </div>
       </div>
-    </div>
+    </section>
   );
 }
