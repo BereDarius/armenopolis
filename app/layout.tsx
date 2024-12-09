da4aac58160d91129660bf87e7cf64686c2d27fe
Darius-Bogdan Bere <darius.bogdan3080@gmail.com>
2024-12-09 06:09:45 +0200

    feature: keywords metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,7 @@ export const metadata: Metadata = {
     template: `%s - ${siteConfig.name}`,
   },
   description: siteConfig.description,
-  icons: {
-    icon: "/favicon.ico",
-  },
+  keywords: siteConfig.keywords,
 };
 
 export const viewport: Viewport = {
