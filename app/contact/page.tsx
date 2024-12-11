b613576eace426949c2e2f5b2278c6462c4a990a
Darius-Bogdan Bere <darius.bogdan3080@gmail.com>
2024-12-11 20:50:47 +0200

    chore: update contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,23 +4,25 @@ export default function ContactPage() {
   return (
     <main className="flex flex-col md:flex-row gap-8 text-left justify-center items-center w-full md:h-4/6 text-sm md:text-2xl md:my-6 md:gap-12">
       <div className="flex flex-col gap-4 md:w-5/6 md:gap-6">
-        <span className="text-2xl md:text-5xl text-default-600">Email</span>
+        <span className="text-2xl md:text-5xl text-default-600">✉️ Email</span>
         <a
           className="text-blue-600 hover:underline"
           href="mailto:gherlaccdp@yahoo.com"
         >
           gherlaccdp@yahoo.com
         </a>
-        <span className="text-2xl md:text-5xl text-default-600">Telefon</span>
+        <span className="text-2xl md:text-5xl text-default-600">
+          📞 Telefon
+        </span>
         <a className="text-blue-600 hover:underline" href="tel:+40723946272">
           0723946272
         </a>
         <section className="flex flex-col md:flex-row gap-4">
           <section className="flex flex-col gap-4 md:gap-6">
             <span className="text-2xl md:text-5xl text-default-600">
-              Adresă
+              📍 Adresă
             </span>
-            <article>Strada Mihai Viteazu 15, Gherla 405300</article>
+            <article>Strada Mihai Viteazu 15, Gherla 405300, România</article>
           </section>
         </section>
       </div>
